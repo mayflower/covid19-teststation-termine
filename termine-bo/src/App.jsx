354043a12956944f61f4aecfc4eabf573380e3c3
Max Tharr <max.tharr@mayflower.de>
2021-03-19 16:43:41 +0100

    Add a router

--- termine-bo/src/App.jsx
+++ termine-bo/src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { UserManagement } from "./Components/UserManagement";
+import {
+  HashRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
-  return <UserManagement />;
-}
-
-export default App;
+export const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/users" />
+      </Route>
+      <Route path="/users">
+        <UserManagement />
+      </Route>
+    </Switch>
+  </Router>
+);
